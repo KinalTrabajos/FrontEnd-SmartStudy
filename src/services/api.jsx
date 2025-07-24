03846421702e1702127cb3f8f438a5fc82c52226
Aldair Alejandro González Araujo <aldairalejandrogonzalezaraujo@gmail.com>
2025-07-24 01:42:07 -0600

    Cambios en tareas

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiStudy = axios.create({
-    baseURL: 'http://localhost:8001/TDAHSystem/v1',
+    baseURL: 'http://localhost:8080/TDAHSystem/v1',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
